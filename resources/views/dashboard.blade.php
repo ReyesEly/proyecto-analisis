3ea80cb834a1e3bbbfc44c1716c8154c4e1868a7
Nery-Reyes <nreyesl@miumg.edu.gt>
2023-11-09 09:06:06 -0600

    agregando estilos cdn

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,11 +26,13 @@
 
 @section('css')
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/css/adminlte.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.18.0/font/bootstrap-icons.css">
     <!-- Agregar otros enlaces CDN de CSS si es necesario -->
 @stop
 
 @section('js')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/js/adminlte.min.js"></script>
     <!-- Agregar otros enlaces CDN de JavaScript si es necesario -->
 @stop
